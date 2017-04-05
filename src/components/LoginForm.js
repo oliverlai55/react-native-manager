6cae9543ef55ff66747c3105e18b2f48ef79ab79
Oliver Lai <oliverlai55@gmail.com>
2017-04-05 15:49:31 -0400

    Add new Login component

---
+++ src/components/LoginForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Card } from './common';
+
+class LoginForm extends Component {
+  render() {
+    return (
+      <Card>
+      </Card>
+    );
+  }
+}

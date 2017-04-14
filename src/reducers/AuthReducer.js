808d10ce747bbec4e3492fff615fdb61fadeb8c2
oliverlai55 <oliverlai55@gmail.com>
2017-04-14 03:30:46 -0400

    Refactor code

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -2,13 +2,16 @@ import {
   EMAIL_CHANGED,
   PASSWORD_CHANGED,
   LOGIN_USER_SUCCESS,
-  LOGIN_USER_FAIL
+  LOGIN_USER_FAIL,
+  LOGIN_USER
 } from '../actions/types';
 
 const INITIAL_STATE = {
   email: '',
   password: '',
-  user: null
+  user: null,
+  error: '',
+  loading: false
 };
 
 export default (state = INITIAL_STATE, action) => {
@@ -17,10 +20,12 @@ export default (state = INITIAL_STATE, action) => {
       return { ...state, email: action.payload };
     case PASSWORD_CHANGED:
       return { ...state, password: action.payload };
+    case LOGIN_USER:
+      return { ...state, loading: true, error: '' }
     case LOGIN_USER_SUCCESS:
-      return { ...state, user: action.payload };
+      return { ...state, user: action.payload, error: '', loading: false };
     case LOGIN_USER_FAIL:
-      return { ...state, error: 'Authentication Failed.', password: '' };
+      return { ...state, error: 'Authentication Failed.', password: '', loading: false };
     default:
       return state;
   }

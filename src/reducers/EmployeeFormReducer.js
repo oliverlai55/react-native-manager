e218d1836cb5f7e1b8b52947dd01918067447533
oliverlai55 <oliverlai55@gmail.com>
2017-04-17 23:17:30 -0400

    Refactor code

---
+++ src/reducers/EmployeeFormReducer.js
@@ -0,0 +1,12 @@
+import {
+  EMPLOYEE_UPDATE
+} from '../actions/types';
+
+const INITAL_STATE = {};
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};

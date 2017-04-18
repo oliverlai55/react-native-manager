8c69a88b1a26acaed26f969308c8d5a2400a991c
oliverlai55 <oliverlai55@gmail.com>
2017-04-17 23:32:26 -0400

    Refactor form

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -2,10 +2,18 @@ import {
   EMPLOYEE_UPDATE
 } from '../actions/types';
 
-const INITAL_STATE = {};
+const INITIAL_STATE = {
+  name: '',
+  phone: '',
+  shift: ''
+};
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case EMPLOYEE_UPDATE:
+      // action.payload === { prop: 'name', value: 'jane' }
+      // [] is key interpolation if input is 'name' then prop is 'name', if its 'shift', its shift
+      return { ...state, [action.payload.prop]: action.payload.value };
     default:
       return state;
   }

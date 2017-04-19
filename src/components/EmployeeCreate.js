e51645286c407d329c9cbd3b9687a8a9f2677860
oliverlai55 <oliverlai55@gmail.com>
2017-04-19 01:37:42 -0400

    Refactor code

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -8,7 +8,7 @@ class EmployeeCreate extends Component {
   onButtonPress() {
     const { name, phone, shift } = this.props;
 
-    this.props.employeeCreate({ name, phone, shift });
+    this.props.employeeCreate({ name, phone, shift: shift || 'Monday' });
   }
 
   render() {
@@ -72,5 +72,5 @@ const mapStateToProps = (state) => {
 };
 
 export default connect(mapStateToProps, {
-  employeeUpdate, employeeCreate 
+  employeeUpdate, employeeCreate
 })(EmployeeCreate);
